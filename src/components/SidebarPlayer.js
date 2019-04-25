b348841e0ac73b066178fcb83e8ec655e5d76c59
Mateus Santos <33528161+uaumateus@users.noreply.github.com>
2019-04-25 15:39:28 -0300

    Navbar e menu fixos

---
+++ src/components/SidebarPlayer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './SidebarPlayer.css';
+class SidebarPlayer extends Component {
+  render() {
+    return (
+      <div className="sidebar">
+        
+      </div>
+    );
+  }
+}
+
+export default SidebarPlayer;
--- src/pages/Player.js
+++ src/pages/Player.js
@@ -1,21 +1,23 @@
 import React, { Component } from 'react';
 import Navbar from '../components/Navbar';
+import SidebarPlayer from '../components/SidebarPlayer';
 import './Player.css';
 import addList from '../assets/icons/add_list.svg';
 import share from '../assets/icons/share_button.svg';
 import applause from '../assets/icons/clap_button.svg';
+
 
 class Player extends Component {
   render() {
     return (
       <div>
         <Navbar/>
-        <iframe src="https://www.youtube.com/embed/bo_efYhYU2A" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         <div className="container">
-          
-          <h1 className="Title-Film">Bird Box</h1>
+          <iframe src="https://www.youtube.com/embed/bo_efYhYU2A" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          {/* <SidebarPlayer/> */}
+          <h1 className="Title-Film title">Bird Box</h1>
           <h1 className="Views-Film">5092 visualizações</h1>
-          <div className="Actions">
+          <div className="Actions ActionsPlayer">
             <img src={addList} className="Icons"/>
             <img src={share} className="Icons"/>
             <div>
