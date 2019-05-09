c3b71b01afaff7c7e5b970df2ce7e178c0e733e8
Paulo José <paulojocb@gmail.com>
2019-05-08 21:41:23 -0300

    Alterações na tela de informação
    - Novo componente para o botão de assisitr
    
     - Fetch as informações do vídeo na tela de informação

--- src/pages/VideoInfo/VideoInfos.js
+++ src/pages/VideoInfo/VideoInfos.js
@@ -1,15 +1,51 @@
 import React, { Component } from 'react';
 
+import './VideoInfos.css';
+
 import * as ROUTES from '../../constants/routes';
 
+import PlayButton from '../../components/Buttons/PlayButton/PlayButton';
 import ActionsPlayer from '../../components/ActionsPlayer/ActionsPlayer';
 import Tags from '../../components/Tags/Tags';
 import ParentalRating from '../../components/ParentalRating/ParentalRating';
 
-import './VideoInfos.css';
+import { withFirebase } from '../../Firebase';
 
 class VideoInfos extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { video: null, loading: false };
+  }
+  componentDidMount() {
+    this.setState({ loading: true });
+    const {
+      firebase,
+      match: { params }
+    } = this.props;
+
+    firebase.video.get(params.videoId, (video, error) => {
+      this.setState({ video, error, loading: false });
+    });
+  }
+
+  componentWillUnmount() {
+    const { firebase } = this.props;
+    firebase.video.turnOff();
+  }
+
+  onPlay = e => {
+    const { video } = this.state;
+    const {
+      match: { params }
+    } = this.props;
+
+    this.props.history.push(`${ROUTES.PLAYER}/${params.videoId}`);
+  };
+
   render() {
+    const { video, loading } = this.state;
+
     return (
       <div className="container VideoInfos">
         <div>
@@ -28,15 +64,10 @@ class VideoInfos extends Component {
             certa: se você olhar, você morre.
           </h1>
 
-          <button
-            className="Play"
-            onClick={() =>
-              this.props.history.push(`${ROUTES.PLAYER}/-LeJafAkmRY9wYnhZKS4`)
-            }
-          >
-            <i className="fas fa-play" />
-            ASSISTIR
-          </button>
+          <PlayButton disabled={loading ? true : false} click={this.onPlay}>
+            Assistir
+          </PlayButton>
+
           <ActionsPlayer />
         </div>
         <div>
@@ -48,4 +79,4 @@ class VideoInfos extends Component {
   }
 }
 
-export default VideoInfos;
+export default withFirebase(VideoInfos);
