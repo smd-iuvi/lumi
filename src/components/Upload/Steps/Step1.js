24a7382f9d2217b226066d87a32a8d36ef3e08c0
Mateus Santos <33528161+uaumateus@users.noreply.github.com>
2019-05-23 20:48:39 -0300

    Adicionados os campos que faltavam no step1

--- src/components/Upload/Steps/Step1.js
+++ src/components/Upload/Steps/Step1.js
@@ -5,6 +5,8 @@ import './Steps.css';
 import Thumbnail from '../Thumbnail/Thumbnail';
 import TextFieldInformation from '../TextFieldInformation/TextFieldInformation';
 import TextAreaInformation from '../TextAreaInformation/TextAreaInformation';
+import SelectBox from '../SelectBox/SelectBox';
+import AddTags from '../AddTags/AddTags';
 
 const Step1 = (props) => {
     return (
@@ -15,6 +17,12 @@ const Step1 = (props) => {
             <TextFieldInformation placeholder="Título do vídeo" />
             <TextFieldInformation placeholder="Link do vídeo" />
             <TextAreaInformation placeholder="Sinopse/Descrição" />
+            <SelectBox name="genre" placeholder="Gênero" />
+            <div className="classificationDiv">
+                <SelectBox name="parentalRating" placeholder="Classificação" />
+                <TextFieldInformation placeholder="Conteúdo" />
+            </div>
+            <AddTags list={false} placeholder="Adicione tags ao seu vídeo" />
         </div>
     );
 };
