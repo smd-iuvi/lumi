24fdbb357f71f5d31324d5db8819241b97f96cfd
Paulo José <paulojocb@gmail.com>
2019-05-24 13:26:47 -0300

    Consertando chamada da funçao de logout

--- src/components/Sidebar/ButtonsBottom/ButtonsBottom.js
+++ src/components/Sidebar/ButtonsBottom/ButtonsBottom.js
@@ -2,11 +2,10 @@ import React from 'react';
 
 import './ButtonsBottom.css';
 
-
 const ButtonsBottom = ({ click, newClass, children }) => {
   return (
-    <article className="buttonsSidebar buttonsBottom">
-      <article className={newClass} ></article>
+    <article className="buttonsSidebar buttonsBottom" onClick={click}>
+      <article className={newClass} />
       <h1 className="labelBottom">{children}</h1>
     </article>
   );
