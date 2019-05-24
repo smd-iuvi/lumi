fe86ed614de794c02d39c6197a7ac7399d175dba
Mateus Santos <33528161+uaumateus@users.noreply.github.com>
2019-05-24 10:30:25 -0300

    Nova função implementada
    * o botão de excluir função ta excluindo o ultimo item da lista, é preciso corrigir isso

--- src/components/Upload/Steps/Step2.js
+++ src/components/Upload/Steps/Step2.js
@@ -1,20 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './Steps.css';
 
 import AddTags from '../AddTags/AddTags';
+import NewInformation from '../NewInformation/NewInformation';
 
-const Step2 = (props) => {
-    return (
-        <div className="Steps">
-            <h1 className="titleStep">Ficha técnica</h1>
-            <article className="line"></article>
-            <h1 className="subtitleStep">Aqui você pode mostrar quem participou da produção do seu vídeo.</h1>
-            <h1 className="titleStep2">Elenco</h1>
-            <AddTags list={true} placeholder="Aperte ENTER para adicionar uma pessoa. Se não possuir elenco, deixe em branco." />
-            <h1 className="titleStep2">Funções dos participantes</h1>
-        </div>
-    );
+import iconNew from './assets/add-function.svg';
+
+class Step2 extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            functions: [{ func: '', name: '' }]
+        }
+        this.addFunction = this.addFunction.bind(this);
+    }
+
+    addFunction = () => {
+        let aux = this.state.functions
+        aux.push({ func: '', name: '' })
+        this.setState({ functions: aux })
+    }
+
+    removeFunction = (value) => {
+        let aux = this.state.functions
+        aux.splice(value, 1)
+        this.setState({ functions: aux })
+    }
+
+    render() {
+        return (
+            <div className="Steps">
+                <h1 className="titleStep">Ficha técnica</h1>
+                <article className="line"></article>
+                <h1 className="subtitleStep">Aqui você pode mostrar quem participou da produção do seu vídeo.</h1>
+                <h1 className="titleStep2">Elenco</h1>
+                <AddTags list={true} placeholder="Aperte ENTER para adicionar uma pessoa. Se não possuir elenco, deixe em branco." />
+                <h1 className="titleStep2">Funções dos participantes</h1>
+                <button className="bntNewInformation" onClick={this.addFunction}>
+                    <img src={iconNew} />
+                    Adicionar função
+            </button>
+                <div className="functions">
+                    {this.state.functions.map((op, index) =>
+                        <NewInformation index={index} removeFunction={this.removeFunction} />
+                    )}
+                </div>
+            </div>
+        );
+    }
 };
 
 export default Step2;
