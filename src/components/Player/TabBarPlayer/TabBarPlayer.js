2e3e932470d8986570dd446e8008709f773af90e
Mateus Santos <33528161+uaumateus@users.noreply.github.com>
2019-05-27 19:48:28 -0300

    Hover na tela de player

--- src/components/Player/TabBarPlayer/TabBarPlayer.js
+++ src/components/Player/TabBarPlayer/TabBarPlayer.js
@@ -3,21 +3,20 @@ import './TabBarPlayer.css';
 
 import VideoInfo from '../VideoInfo/VideoInfo';
 
-import back from './assets/back.svg';
 import next from './assets/next.svg';
 import film from '../../../assets/birdbox.jpg';
 
 const TabBarPlayer = () => {
     return (
         <div className="TabBarPlayer">
-            <img src={back} className="iconBack" />
+            <article className="iconBack" />
             <div>
                 <VideoInfo />
             </div>
             <div className="containerNextFilm">
                 <h1 className="nextVideo">Assistir ao próximo vídeo</h1>
-                <article><img src={film} /></article>
-                <img src={next} />
+                <article className="imgNextVideo"><img src={film} /></article>
+                <article className="iconNext" />
             </div>
         </div>
     );
