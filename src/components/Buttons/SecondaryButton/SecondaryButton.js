e7dc33928832342a562d9c271364b2ccb2aff19c
Paulo José <paulojocb@gmail.com>
2019-05-28 19:41:05 -0300

    Estiliza os botões de acordo com seu estado

--- src/components/Buttons/SecondaryButton/SecondaryButton.js
+++ src/components/Buttons/SecondaryButton/SecondaryButton.js
@@ -2,8 +2,14 @@ import React from 'react';
 
 import './SecondaryButton.css';
 
-const SecondaryButton = ({ children }) => {
-  return <div className="SecondaryButton">{children}</div>;
+const SecondaryButton = ({ children, onClick, enabled = true }) => {
+  const modifierClass = enabled ? 'enabled' : 'disabled';
+  const classes = `SecondaryButton ${modifierClass}`;
+  return (
+    <div className={classes} onClick={enabled ? onClick : null}>
+      {children}
+    </div>
+  );
 };
 
 export default SecondaryButton;
