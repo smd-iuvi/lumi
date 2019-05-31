0f8d82311e7100959e6259a9e620fa5b6dc1da6c
Paulo José <paulojocb@gmail.com>
2019-05-31 17:31:21 -0300

    Update Step2.js

--- src/components/Upload/Steps/Step2.js
+++ src/components/Upload/Steps/Step2.js
@@ -8,47 +8,62 @@ import NewInformation from '../NewInformation/NewInformation';
 import iconNew from './assets/add-function.svg';
 
 class Step2 extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            functions: [{ func: '', name: '' }]
-        }
-        this.addFunction = this.addFunction.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      functions: [{ func: '', name: '' }]
+    };
+    this.addFunction = this.addFunction.bind(this);
+  }
 
-    addFunction = () => {
-        let aux = this.state.functions
-        aux.push({ func: '', name: '' })
-        this.setState({ functions: aux })
-    }
+  addFunction = () => {
+    let aux = this.state.functions;
+    aux.push({ func: '', name: '' });
+    this.setState({ functions: aux });
+  };
 
-    removeFunction = (value) => {
-        let aux = this.state.functions
-        aux.splice(value, 1)
-        this.setState({ functions: aux })
-    }
+  removeFunction = value => {
+    let aux = this.state.functions;
+    aux.splice(value, 1);
+    this.setState({ functions: aux });
+  };
 
-    render() {
-        return (
-            <div className="Steps">
-                <h1 className="Large-Text-Medium">Ficha técnica</h1>
-                <article className="line"></article>
-                <h1 className="subtitleStep Small-Text-Regular">Aqui você pode mostrar quem participou da produção do seu vídeo.</h1>
-                <h1 className="Medium-Text-Medium">Elenco</h1>
-                <AddTags list={true} placeholder="Aperte ENTER para adicionar uma pessoa. Se não possuir elenco, deixe em branco." />
-                <h1 className="Medium-Text-Medium">Funções dos participantes</h1>
-                <button className="bntNewInformation Small-Text-Bold" onClick={this.addFunction}>
-                    <img src={iconNew} />
-                    Adicionar função
-                </button>
-                <div className="functions">
-                    {this.state.functions.map((op, index) =>
-                        <NewInformation index={index} removeFunction={this.removeFunction} />
-                    )}
-                </div>
-            </div>
-        );
-    }
-};
+  render() {
+    const { stepState, onChange } = this.props;
+    return (
+      <div className="Steps">
+        <h1 className="Large-Text-Medium">Ficha técnica</h1>
+        <article className="line" />
+        <h1 className="subtitleStep Small-Text-Regular">
+          Aqui você pode mostrar quem participou da produção do seu vídeo.
+        </h1>
+        <h1 className="Medium-Text-Medium">Elenco</h1>
+        <AddTags
+          name="cast"
+          value={stepState.cast.value}
+          onChange={onChange}
+          list={true}
+          placeholder="Aperte ENTER para adicionar uma pessoa. Se não possuir elenco, deixe em branco."
+        />
+        <h1 className="Medium-Text-Medium">Funções dos participantes</h1>
+        <button
+          className="bntNewInformation Small-Text-Bold"
+          onClick={this.addFunction}
+        >
+          <img src={iconNew} />
+          Adicionar função
+        </button>
+        <div className="functions">
+          {this.state.functions.map((op, index) => (
+            <NewInformation
+              index={index}
+              removeFunction={this.removeFunction}
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Step2;
