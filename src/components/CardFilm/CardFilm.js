e6b352628a43699b7c0f051e85e2678d3d7b458c
Paulo José <paulojocb@gmail.com>
2019-06-01 18:48:32 -0300

    Coloca imagem da ulr no card

--- src/components/CardFilm/CardFilm.js
+++ src/components/CardFilm/CardFilm.js
@@ -7,14 +7,20 @@ import * as ROUTES from '../../constants/routes';
 import card from '../../assets/birdbox.jpg';
 
 const CardFilm = ({ video }) => {
+  const style = {
+    backgroundImage: `url(${video.imageUrl ? video.imageUrl : card})`
+  };
   return (
     <Link
       to={`${ROUTES.PLAYER}/${video.uid}`}
       style={{ textDecoration: 'none' }}
     >
       <div className="cardFilm">
-        <article className="containerImgCard">
-          <img src={card} className="imgCard" />
+        <article className="containerImgCard" style={style}>
+          {/* <img
+            src={video.imageUrl ? video.imageUrl : card}
+            className="imgCard"
+          /> */}
         </article>
         <div className="infosCard">
           <h1 className="Medium-Text-Bold">
