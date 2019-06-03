91934c55dad6cb95557b4ed28b0226172bc6264c
Mateus Santos <33528161+uaumateus@users.noreply.github.com>
2019-06-03 18:49:15 -0300

    Botão de criar evento no perfil do professor

--- src/components/TabBar/TabBar.js
+++ src/components/TabBar/TabBar.js
@@ -5,6 +5,17 @@ import Header from '../Header/Header';
 import Tabs from './Tabs/Tabs';
 
 class TabBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      profileTeacher: null
+    }
+  }
+
+  componentDidMount() {
+    this.setState({ profileTeacher: this.props.profileTeacher });
+  }
+
   render() {
     return (
       <div className="tabBar">
@@ -14,7 +25,15 @@ class TabBar extends Component {
           </article>
           <Header>{this.props.title}</Header>
         </div>
-        <Tabs tabs={this.props.tabs} onTabChange={this.props.onTabChange} />
+        {this.state.profileTeacher ? (
+          <div className="tabTeacher">
+            <Tabs tabs={this.props.tabs} onTabChange={this.props.onTabChange} />
+            <button className="button buttonPrimary">Criar evento</button>
+          </div>
+        ) : (
+            <Tabs tabs={this.props.tabs} onTabChange={this.props.onTabChange} />
+          )}
+
       </div>
     );
   }
