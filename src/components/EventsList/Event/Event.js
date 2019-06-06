59adb660248b4cedc57f9b7b8e5ddd85e2ab902d
Paulo José <paulojocb@gmail.com>
2019-06-06 14:35:27 -0300

    Lista evento na home

--- src/components/EventsList/Event/Event.js
+++ src/components/EventsList/Event/Event.js
@@ -3,20 +3,21 @@ import React, { Component } from 'react';
 import './Event.css';
 
 class Event extends Component {
-    render() {
-        return (
-            <div className="Event">
-                <div className="eventDate">
-                    <h1 className="Event-Day">18</h1>
-                    <h1 className="Event-Month">ABR</h1>
-                </div>
-                <div>
-                    <h1 className="Medium-Text-Bold">Curtas animados para crianças com autismo</h1>
-                    <h1 className="Medium-Text-Regular">Produção Audiovisual para Crianças e Adolescentes</h1>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    const { event } = this.props;
+    return (
+      <div className="Event">
+        <div className="eventDate">
+          <h1 className="Event-Day">{new Date(event.date).getDay()}</h1>
+          <h1 className="Event-Month">{new Date(event.date).getMonth()}</h1>
+        </div>
+        <div>
+          <h1 className="Medium-Text-Bold">{event.name}</h1>
+          <h1 className="Medium-Text-Regular">{event.description}</h1>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Event;
--- src/components/EventsList/EventsList.js
+++ src/components/EventsList/EventsList.js
@@ -1,11 +1,36 @@
-import React from 'react'
+import React from 'react';
 
 import Event from './Event/Event';
 import Header from '../Header/Header';
 
 import './EventsList.css';
 
-const EventsList = () => {
+const EventsList = ({ events }) => {
+  let firstList = null;
+  let secondList = null;
+
+  if (events) {
+    const monthIndex = events.reduce((accumulator, event) => {
+      return new Date(event.date).getMonth() < accumulator
+        ? new Date(event.date).getMonth()
+        : accumulator;
+    }, 12);
+
+    console.log(monthIndex);
+
+    firstList = events.filter(
+      event => new Date(event.date).getMonth() === monthIndex
+    );
+
+    secondList = events.filter(
+      event => new Date(event.date).getMonth() > monthIndex
+    );
+
+    console.log(secondList);
+  }
+
+  // const first
+
   return (
     <div className="EventsList">
       <div className="titleEventsFilms">
@@ -14,12 +39,15 @@ const EventsList = () => {
       </div>
       <article className="line" />
       <h1 className="Month Small-Text-Bold">NESTE MÊS</h1>
-      <Event />
-      <Event />
-      <h1 className="Month Small-Text-Bold">PRÓXIMOS MESES</h1>
-      <Event />
+      {firstList && firstList.map(event => <Event event={event} />)}
+      {secondList && secondList.length > 0 && (
+        <h1 className="Month Small-Text-Bold">PRÓXIMOS MESES</h1>
+      )}
+      {secondList &&
+        secondList.length > 0 &&
+        secondList.map(event => <Event event={event} />)}
     </div>
-  )
-}
+  );
+};
 
-export default EventsList
+export default EventsList;
