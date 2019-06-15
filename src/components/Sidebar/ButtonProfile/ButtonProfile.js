cc68ccf6096c9c1bf36dda9672dfb6b6396fb784
Mateus Santos <33528161+uaumateus@users.noreply.github.com>
2019-06-15 19:01:06 -0300

    Redimensionando imagens

--- src/components/Sidebar/ButtonProfile/ButtonProfile.js
+++ src/components/Sidebar/ButtonProfile/ButtonProfile.js
@@ -6,7 +6,7 @@ import './ButtonProfile.css';
 const ButtonProfile = (props) => {
     return (
         <article className="buttonsSidebar buttonProfile">
-            <img src={props.image} className="imgProfile" />
+            <div className="imgProfile"><img src={props.image} /></div>
             <h1 className="Medium-Text-Regular">{props.children}</h1>
         </article>
     );
