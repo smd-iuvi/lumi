935529a61b8e9823d67a02dac64b1d33e6529c73
Mateus Santos <33528161+uaumateus@users.noreply.github.com>
2019-06-16 20:34:41 -0300

    Landing page

--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -1,17 +1,35 @@
 import React from 'react';
+import './Landing.css';
 import { Link } from 'react-router-dom';
-
 import * as ROUTES from '../../constants/routes';
 
-const style = {
-  color: '#fff'
-};
+import logo from './assets/lumi.svg';
 
 const Landing = () => {
   return (
-    <div className="container" style={style}>
-      <h1>Landing Page</h1>
-      <Link to={ROUTES.SIGN_IN}>Logar </Link>
+    <div className="landing">
+      <div className="landingNavbar">
+        <Link to={ROUTES.SIGN_UP} className="link">
+          <h1 className="Large-Text-Bold buttonRegister">Cadastre-se</h1>
+        </Link>
+        <Link to={ROUTES.SIGN_IN} className="link">
+          <h1 className="Large-Text-Bold">Entrar</h1>
+        </Link>
+      </div>
+
+      <div className="landingContent">
+        <img src={logo} className="landingLogo" />
+        <h1 className="Large-Text-Regular">Frase topperson sobre o lumi</h1>
+        <Link to={ROUTES.HOME} className="link">
+          <button className="button buttonPrimary">Conheça agora</button>
+        </Link>
+      </div>
+
+      <div className="landingNetworks">
+        <article><i class="fab fa-facebook-f"></i></article>
+        <article><i class="fab fa-instagram"></i></article>
+        <article><i class="fas fa-globe"></i></article>
+      </div>
     </div>
   );
 };
