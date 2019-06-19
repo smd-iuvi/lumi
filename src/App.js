29c19371d8f45858e4c493e4e9be8d6683cc627a
Mateus Santos <33528161+uaumateus@users.noreply.github.com>
2019-06-18 21:31:03 -0300

    Esquema de reconhecimento da largura da tela

--- src/App.js
+++ src/App.js
@@ -24,49 +24,74 @@ import AdminDashboard from './pages/AdminDashboard/AdminDashboard';
 import { withAuthentification } from './Firebase/Session';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { width: 0 };
+    this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+  }
+
+  componentDidMount() {
+    this.updateWindowDimensions();
+    window.addEventListener('resize', this.updateWindowDimensions);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateWindowDimensions);
+  }
+
+  updateWindowDimensions() {
+    this.setState({ width: window.innerWidth });
+  }
   render() {
     const { location } = this.props;
 
     return (
       <div>
-        {!location.pathname.includes(ROUTES.PLAYER)
-          && !location.pathname.includes(ROUTES.LANDING)
-          ? <Sidebar /> : null}
+        {this.state.width > 1170 ?
+          (
+            <>
+              {!location.pathname.includes(ROUTES.PLAYER)
+                && !location.pathname.includes(ROUTES.LANDING)
+                ? <Sidebar /> : null}
 
 
-        {!location.pathname.includes(ROUTES.LANDING)
-          ? <NavBar class="navbar" /> : null}
-        {location.pathname.includes(ROUTES.PLAYER)
-          && <NavBar class="navbar navbarComplete" />}
+              {!location.pathname.includes(ROUTES.LANDING)
+                ? <NavBar class="navbar" /> : null}
+              {location.pathname.includes(ROUTES.PLAYER)
+                && <NavBar class="navbar navbarComplete" />}
 
-        <Switch>
-          <Route path={ROUTES.HOME} exact component={Home} />
-          <Route
-            path={`${ROUTES.VIDEO}/:videoId`}
-            exact
-            component={VideoInfos}
-          />
-          <Route path={`${ROUTES.PLAYER}/:videoId`} component={Player} />
-          <Route path={ROUTES.PROFILE} exact component={Profile} />
-          <Route path={ROUTES.UPLOAD} exact component={Upload} />
-          <Route path={ROUTES.SIGN_IN} exact component={SignIn} />
-          <Route path={ROUTES.SIGN_UP} exact component={SignUp} />
-          <Route path={ROUTES.LANDING} exact component={Landing} />
-          <Route path={ROUTES.ADMIN} exact component={AdminDashboard} />
-          <Route
-            path={`${ROUTES.SEARCH}/:searchTerm`}
-            exact
-            component={Search}
-          />
-          <Route path={ROUTES.EVENT} exact component={Event} />
-          <Route path={ROUTES.CATEGORY} exact component={Category} />
-          <Route path={ROUTES.RESTRICTED_AREA} exact component={RestrictedArea} />
-          <Route component={Error404} />
-        </Switch>
+              <Switch>
+                <Route path={ROUTES.HOME} exact component={Home} />
+                <Route
+                  path={`${ROUTES.VIDEO}/:videoId`}
+                  exact
+                  component={VideoInfos}
+                />
+                <Route path={`${ROUTES.PLAYER}/:videoId`} component={Player} />
+                <Route path={ROUTES.PROFILE} exact component={Profile} />
+                <Route path={ROUTES.UPLOAD} exact component={Upload} />
+                <Route path={ROUTES.SIGN_IN} exact component={SignIn} />
+                <Route path={ROUTES.SIGN_UP} exact component={SignUp} />
+                <Route path={ROUTES.LANDING} exact component={Landing} />
+                <Route path={ROUTES.ADMIN} exact component={AdminDashboard} />
+                <Route
+                  path={`${ROUTES.SEARCH}/:searchTerm`}
+                  exact
+                  component={Search}
+                />
+                <Route path={ROUTES.EVENT} exact component={Event} />
+                <Route path={ROUTES.CATEGORY} exact component={Category} />
+                <Route path={ROUTES.RESTRICTED_AREA} exact component={RestrictedArea} />
+                <Route component={Error404} />
+              </Switch>
 
-        {!location.pathname.includes(ROUTES.PLAYER)
-          && !location.pathname.includes(ROUTES.LANDING)
-          ? <Footer /> : null}
+              {!location.pathname.includes(ROUTES.PLAYER)
+                && !location.pathname.includes(ROUTES.LANDING)
+                ? <Footer /> : null}
+            </>
+          ) : (
+            console.log("pagina de erro de dimensão")
+          )}
       </div>
     );
   }
