2e7708361b4afb786b48fc14fe3ea801d23c4e76
Mateus Santos <33528161+uaumateus@users.noreply.github.com>
2019-06-20 22:47:43 -0300

    Links do footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,6 +2,9 @@ import React from 'react'
 
 import './Footer.css';
 
+import * as ROUTES from '../../constants/routes';
+import { Link } from 'react-router-dom';
+
 import ufc from './assets/ufc.png';
 import lumi from './assets/lumi.svg';
 import smd from './assets/smd.png';
@@ -14,16 +17,25 @@ const Footer = () => {
       <div className="optionsFooter">
         <div>
           <h1 className="Medium-Text-Bold">Recursos</h1>
-          <h1 className="Medium-Text-Regular">Descobrir</h1>
+          <Link to={ROUTES.DISCOVER} className="link">
+            <h1 className="Medium-Text-Regular">Descobrir</h1>
+          </Link>
           <h1 className="Medium-Text-Regular">Sobre o Lumi</h1>
-          <h1 className="Medium-Text-Regular">Site do SMD</h1>
-          <h1 className="Medium-Text-Regular">Site da UFC</h1>
+          <a href="http://www.smd.ufc.br/pt/" target="blink">
+            <h1 className="Medium-Text-Regular">Site do SMD</h1>
+          </a>
+          <a href="http://www.ufc.br/" target="blink">
+            <h1 className="Medium-Text-Regular">Site da UFC</h1>
+          </a>
         </div>
         <div>
           <h1 className="Medium-Text-Bold">Conta</h1>
-          <h1 className="Medium-Text-Regular">Minha conta</h1>
-          <h1 className="Medium-Text-Regular">Minha lista</h1>
-          <h1 className="Medium-Text-Regular">Vídeos enviados</h1>
+          <Link to={ROUTES.PROFILE} className="link">
+            <h1 className="Medium-Text-Regular">Minha conta</h1>
+          </Link>
+          <Link to={ROUTES.PROFILE_MY_LIST} className="link">
+            <h1 className="Medium-Text-Regular">Minha lista</h1>
+          </Link>
         </div>
         <div>
           <h1 className="Medium-Text-Bold">Suporte</h1>
@@ -38,14 +50,18 @@ const Footer = () => {
           </div>
           <h1 className="Medium-Text-Bold">Redes sociais</h1>
           <div className="networks logos">
-            <article>
-              <img src={facebook} />
-              <h1 className="titleNetwork Medium-Text-Regular">@SMDUFC</h1>
-            </article>
-            <article>
-              <img src={instagram} />
-              <h1 className="titleNetwork Medium-Text-Regular">@smdufc</h1>
-            </article>
+            <a href="https://www.facebook.com/SMDUFC/" target="blink">
+              <article>
+                <img src={facebook} />
+                <h1 className="titleNetwork Medium-Text-Regular">@SMDUFC</h1>
+              </article>
+            </a>
+            <a href="https://www.instagram.com/ca.smd/" target="blink">
+              <article>
+                <img src={instagram} />
+                <h1 className="titleNetwork Medium-Text-Regular">@ca.smd</h1>
+              </article>
+            </a>
           </div>
         </div>
       </div>
