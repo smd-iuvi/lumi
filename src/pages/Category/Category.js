44837405b39dfdf8a13aad4966f86c62933348a4
Paulo José <paulojocb@gmail.com>
2019-06-21 11:14:51 -0300

    Linka cards de disciplnas com página da disciplina

--- src/pages/Category/Category.js
+++ src/pages/Category/Category.js
@@ -1,68 +1,108 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
 import './Category.css';
 
 import TabBar from '../../components/TabBar/TabBar';
 import CardList from '../../components/CardList/CardList';
 
+import { withFirebase } from '../../Firebase';
+
+import { QueryableFields as Video } from '../../Firebase/Models/Video';
+
 import iconDiscipline from './assets/discipline.svg';
 import iconSemester from './assets/semester.svg';
 
 class Category extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            category: 'discipline' //discipline or semester
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      category: 'discipline', //discipline or semester
+      videos: null,
+      loading: false
+    };
+  }
 
-    render() {
-        let container = null;
+  componentDidMount() {
+    const {
+      firebase,
+      match: { params }
+    } = this.props;
 
-        if (this.state.category === 'discipline') {
-            container = (
-                <>
-                    <div className="categoryInfos">
-                        <div>
-                            <h1 className="Medium-Text-Regular">A Educomunicação é um campo interdisciplinar em grande ascensão mundialmente, ocupando-se dos processos dialógicos do ato educativo. O profissional de Sistemas e Mídias Digitais de hoje, que
-transita em várias áreas (educação formal, não formal, informal), deve saber transcender a instrumentalidade técnica, promovendo a conversão da comunicação em processo educativo.</h1>
-                        </div>
-                        <article></article>
-                    </div>
-                    <article className="line"></article>
-                    <h1 className="Small-Text-Bold titleSectionCategory">TRABALHOS DESTA DISCIPLINA</h1>
-                </>
-            );
-        } else if (this.state.category === 'semester') {
-            container = <h1 className="Small-Text-Bold titleSectionCategory titleSectionSemester">TRABALHOS REALIZADOS EM 2019.1</h1>;
-        }
+    this.setState({ loading: true });
 
-        return (
-            <div className="Category">
-                {this.state.category === 'discipline' &&
-                    <TabBar
-                        icon={iconDiscipline}
-                        title="Disciplina"
-                        tabs={null}
-                        onTabChange={null}
-                        profileTeacher={false}
-                    />
-                }
-                {this.state.category === 'semester' &&
-                    <TabBar
-                        icon={iconSemester}
-                        title="Semestre"
-                        tabs={null}
-                        onTabChange={null}
-                        profileTeacher={false}
-                    />
-                }
-                <div className="container">
-                    {container}
-                    <CardList videos={null} loading={false} belowTab={true} />
-                </div>
+    firebase.video
+      .getVideosBy(Video.DISCIPLINE, params.name)
+      .then(videos => {
+        this.setState({ videos, loading: false });
+      })
+      .catch(error => this.setState({ error, loading: false }));
+  }
+
+  render() {
+    const {
+      match: { params }
+    } = this.props;
+    const { videos, loading } = this.state;
+    let container = null;
+
+    if (this.state.category === 'discipline') {
+      container = (
+        <>
+          <div className="categoryInfos">
+            <div>
+              <h1 className="Medium-Text-Regular">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </h1>
             </div>
-        );
+            <article />
+          </div>
+          <article className="line" />
+          <h1 className="Small-Text-Bold titleSectionCategory">
+            TRABALHOS DESTA DISCIPLINA
+          </h1>
+        </>
+      );
+    } else if (this.state.category === 'semester') {
+      container = (
+        <h1 className="Small-Text-Bold titleSectionCategory titleSectionSemester">
+          TRABALHOS REALIZADOS EM 2019.1
+        </h1>
+      );
     }
+
+    return (
+      <div className="Category">
+        {this.state.category === 'discipline' && (
+          <TabBar
+            icon={iconDiscipline}
+            title={params.name}
+            tabs={null}
+            onTabChange={null}
+            profileTeacher={false}
+          />
+        )}
+        {this.state.category === 'semester' && (
+          <TabBar
+            icon={iconSemester}
+            title="Semestre"
+            tabs={null}
+            onTabChange={null}
+            profileTeacher={false}
+          />
+        )}
+        <div className="container">
+          {container}
+          <CardList videos={videos} loading={loading} belowTab={false} />
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Category;
+export default withFirebase(withRouter(Category));
--- src/pages/Discover/Discover.js
+++ src/pages/Discover/Discover.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import './Discover.css';
 
 import 'react-responsive-carousel/lib/styles/carousel.css';
@@ -6,132 +7,146 @@ import { Carousel } from 'react-responsive-carousel';
 
 import { withFirebase } from '../../Firebase';
 
+import * as ROUTES from '../../constants/routes';
+
 import img from '../../assets/birdbox.jpg';
 
 class Discover extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            autoplay: false,
-            disciplineDataSource: [],
-            semesterDataSource: []
-        };
-        this.randomVideos = this.randomVideos.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      autoplay: false,
+      disciplineDataSource: [],
+      semesterDataSource: []
+    };
+    this.randomVideos = this.randomVideos.bind(this);
+  }
 
-    componentDidMount() {
-        const { firebase } = this.props;
+  componentDidMount() {
+    const { firebase } = this.props;
 
-        firebase.discipline
-            .get()
-            .then(disciplines => {
-                const disciplineDataSource = disciplines.map(
-                    discipline => discipline.name
-                );
-                this.setState({ disciplineDataSource });
-            })
-            .catch(error => {
-                this.setState({ error });
-            });
+    firebase.discipline
+      .get()
+      .then(disciplines => {
+        const disciplineDataSource = disciplines;
+        this.setState({ disciplineDataSource });
+      })
+      .catch(error => {
+        this.setState({ error });
+      });
 
-        firebase.semester
-            .get()
-            .then(semesters => {
-                const semesterDataSource = semesters.map(semester => semester.name);
-                this.setState({ semesterDataSource });
-            })
-            .catch(error => {
-                this.setState({ error });
-            });
-    }
+    firebase.semester
+      .get()
+      .then(semesters => {
+        const semesterDataSource = semesters.map(semester => semester.name);
+        this.setState({ semesterDataSource });
+      })
+      .catch(error => {
+        this.setState({ error });
+      });
+  }
 
-    randomVideos = () => {
-        this.setState({ autoplay: true });
-        setTimeout(function () {
-            this.setState({ autoplay: false });
-        }
-            .bind(this), 3000);
-    }
+  randomVideos = () => {
+    this.setState({ autoplay: true });
+    setTimeout(
+      function() {
+        this.setState({ autoplay: false });
+      }.bind(this),
+      3000
+    );
+  };
 
-    render() {
+  render() {
+    return (
+      <div className="container discover">
+        <h1 className="Large-Text-Bold">
+          Não sabe o que assistir? A gente escolhe por você.
+        </h1>
+        <Carousel
+          showStatus={false}
+          showIndicators={false}
+          showThumbs={false}
+          centerMode
+          centerSlidePercentage={20}
+          showArrows={false}
+          infiniteLoop
+          autoPlay={this.state.autoplay}
+          transitionTime={300}
+          interval={300}
+          selectedItem={3}
+          className="carouselRandom"
+        >
+          <div>
+            <img src={img} />
+          </div>
+          <div>
+            <img src={img} />
+          </div>
+          <div>
+            <img src={img} />
+          </div>
+          <div>
+            <img src={img} />
+          </div>
+          <div>
+            <img src={img} />
+          </div>
+          <div>
+            <img src={img} />
+          </div>
+          <div>
+            <img src={img} />
+          </div>
+          <div>
+            <img src={img} />
+          </div>
+          <div>
+            <img src={img} />
+          </div>
+        </Carousel>
+        <button className="button buttonPrimary" onClick={this.randomVideos}>
+          Iniciar
+        </button>
 
-        return (
-            <div className="container discover">
-                <h1 className="Large-Text-Bold">Não sabe o que assistir? A gente escolhe por você.</h1>
-                <Carousel
-                    showStatus={false}
-                    showIndicators={false}
-                    showThumbs={false}
-                    centerMode
-                    centerSlidePercentage={20}
-                    showArrows={false}
-                    infiniteLoop
-                    autoPlay={this.state.autoplay}
-                    transitionTime={300}
-                    interval={300}
-                    selectedItem={3}
-                    className="carouselRandom"
-                >
-                    <div>
-                        <img src={img} />
-                    </div>
-                    <div>
-                        <img src={img} />
-                    </div>
-                    <div>
-                        <img src={img} />
-                    </div>
-                    <div>
-                        <img src={img} />
-                    </div>
-                    <div>
-                        <img src={img} />
-                    </div>
-                    <div>
-                        <img src={img} />
-                    </div>
-                    <div>
-                        <img src={img} />
-                    </div>
-                    <div>
-                        <img src={img} />
-                    </div>
-                    <div>
-                        <img src={img} />
-                    </div>
-                </Carousel>
-                <button className="button buttonPrimary" onClick={this.randomVideos}>Iniciar</button>
-
-                <div className="categories">
-                    <div>
-                        <h1 className="Large-Text-Bold">Disciplinas</h1>
-                        <article className="line"></article>
-                        <div className="containerCategories">
-                            {this.state.disciplineDataSource.map((op, index) => (
-                                <>
-                                    {index !== 0 &&
-                                        <article className="Small-Text-Regular category">{op}</article>
-                                    }
-                                </>
-                            ))}
-                        </div>
-                    </div>
-                    <div>
-                        <h1 className="Large-Text-Bold">Semestres</h1>
-                        <article className="line"></article>
-                        <div className="containerCategories">
-                            {this.state.semesterDataSource.reverse().map(op => (
-                                <article className="Small-Text-Regular category">{op}</article>
-                            ))}
-                        </div>
-                    </div>
-                    <div>
-
-                    </div>
-                </div>
+        <div className="categories">
+          <div>
+            <h1 className="Large-Text-Bold">Disciplinas</h1>
+            <article className="line" />
+            <div className="containerCategories">
+              {this.state.disciplineDataSource.map((discipline, index) => (
+                <>
+                  {index !== 0 && (
+                    <Link
+                      to={`${ROUTES.CATEGORY}/${discipline.name}`}
+                      className="category"
+                      style={{ textDecoration: 'none', color: 'white' }}
+                    >
+                      <article
+                        className="Small-Text-Regular"
+                        style={{ textDecoration: 'none' }}
+                      >
+                        {discipline.name}
+                      </article>
+                    </Link>
+                  )}
+                </>
+              ))}
             </div>
-        );
-    }
+          </div>
+          <div>
+            <h1 className="Large-Text-Bold">Semestres</h1>
+            <article className="line" />
+            <div className="containerCategories">
+              {this.state.semesterDataSource.reverse().map(op => (
+                <article className="Small-Text-Regular category">{op}</article>
+              ))}
+            </div>
+          </div>
+          <div />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default withFirebase(Discover);
