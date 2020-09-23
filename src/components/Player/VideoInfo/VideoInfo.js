78a58a834f8358e62313670a0c3015f4d4c2cb44
Mateus Santos <mateus.pereira99@hotmail.com>
2020-09-23 18:39:28 -0300

    showing content

--- src/components/Player/VideoInfo/VideoInfo.js
+++ src/components/Player/VideoInfo/VideoInfo.js
@@ -6,20 +6,21 @@ import ParentalRating from '../ParentalRating/ParentalRating';
 const VideoInfo = ({
   title = 'Indisponível',
   discipline = 'Vídeo independente',
-  semester = 'Indisponível',
-  genre = 'Sem gênero',
-  parentalRating = 'Livre'
+  semester,
+  genre,
+  parentalRating,
+  content
 }) => {
   return (
     <div className="VideoInfo">
       <h1 className="Large-Text-Bold">{title}</h1>
       <div>
-        <h1 className="disciplineName Small-Text-Bold">{discipline}</h1>
-        <h1 className="Small-Text-Regular">{semester}</h1>
-        <h1 className="Small-Text-Regular">{genre}</h1>
+        {discipline && <h1 className="disciplineName Small-Text-Bold">{discipline}</h1>}
+        {semester && <h1 className="Small-Text-Regular">{semester}</h1>}
+        {genre && <h1 className="Small-Text-Regular">{genre}</h1>}
         <ParentalRating
           age={parentalRating}
-          content="Conteúdo sexual, consumo de drogas lícitas e violência."
+          content={content}
         />
       </div>
     </div>
