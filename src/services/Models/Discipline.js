ddcb21876c754c3c3c472ce6072a61ed2d1a28cd
Paulo Barbosa <paulo.barbosa@hp.com>
2020-11-12 12:22:22 -0300

    Update Discipline to use ApiManager

--- src/services/Models/Discipline.js
+++ src/services/Models/Discipline.js
@@ -1,94 +1,59 @@
+import { ENDPOINT } from '../ApiManager'
+import ApiManager from '../ApiManager'
+
 class Discipline {
-  constructor(database) {
-    this.database = database;
+  constructor() {
+    this.apiManager = new ApiManager();
   }
 
   create = discipline => {
     return new Promise((resolve, reject) => {
-      this.database
-        .ref('discipline')
-        .push({ ...discipline })
+      this.apiManager.post(`${ENDPOINT.COURSER}`, discipline)
         .then(() => resolve())
-        .catch(error => reject(error));
-    });
+        .catch((err) => reject(err))
+    })
   };
 
   get = (uid = null) => {
     if (uid == null) {
       return new Promise((resolve, reject) => {
-        this.database
-          .ref('discipline')
-          .once('value')
-          .then(snapshot => {
-            const value = snapshot.val();
-
-            if (value != null) {
-              const valueList = Object.keys(value).map(key => ({
-                ...value[key],
-                uid: key
-              }));
-
-              resolve(valueList);
-            } else {
-              resolve(null);
-            }
-          })
-          .catch(error => {
-            reject(error);
-          });
-      });
+        this.apiManager.get(ENDPOINT.COURSER)
+          .then(disciplines => resolve(disciplines))
+          .catch(err => reject(err))
+      })
     } else {
       return new Promise((resolve, reject) => {
-        this.database
-          .ref('discipline')
-          .once('value')
-          .then(snapshot => {
-            resolve(snapshot.val());
-          })
-          .catch(error => {
-            reject(error);
-          });
-      });
+        this.apiManager.get(`${ENDPOINT.COURSER} / ${uid}`)
+          .then(disciplines => resolve(disciplines))
+          .catch(err => reject(err))
+      })
     }
   };
 
-  update = (uid, discipline) =>
-    this.get(uid).set({
-      ...discipline
-    });
+  update = (uid, discipline) => {
+    this.apiManager.put(`${ENDPOINT.COURSER}/${uid}`, discipline)
+      .then(response => { })
+      .catch(err => { })
+  }
 
-  delete = uid => this.get(uid).remove();
+  delete = uid => {
+    this.apiManager.delete(`${ENDPOINT.COURSER}/${uid}`)
+      .then(response => {
+        callback(null)
+      })
+      .catch(err => console.log(err))
+  }
 
+  //Implement Promise 
   getByName = name => {
+    //TODO implement route to get discipline by name
     return new Promise((resolve, reject) => {
-      this.database
-        .ref('discipline')
-        .once('value')
-        .then(snapshot => {
-          const disciplines = snapshot.val();
-
-          if (disciplines != null) {
-            const disciplinesList = Object.keys(disciplines).map(key => ({
-              ...disciplines[key],
-              uid: key
-            }));
-
-            const disciplinesToReturn = disciplinesList.filter(discipline => {
-              return discipline.name.toLowerCase().includes(name.toLowerCase());
-            });
-
-            resolve(
-              disciplinesToReturn.length == 0 ? null : disciplinesToReturn
-            );
-          } else {
-            resolve(null);
-          }
-        })
-        .catch(error => {
-          reject(error);
-        });
+      this.apiManager.get(`${ENDPOINT.COURSER}?name=${name}`)
+        .then(discipline => resolve(discipline))
+        .catch(err => reject(err))
     });
   };
+
 }
 
 export default Discipline;
