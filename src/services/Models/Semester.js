bdcfaa2fa3a38d8dfa49ab126ef6b0e256afeab8
Paulo Barbosa <paulo.barbosa@hp.com>
2020-11-12 12:22:43 -0300

    Update Semester to use ApiManager

--- src/services/Models/Semester.js
+++ src/services/Models/Semester.js
@@ -1,58 +1,49 @@
+import { ENDPOINT } from '../ApiManager'
+import ApiManager from '../ApiManager'
+
 class Semester {
-  constructor(database) {
-    this.database = database;
+  constructor() {
+    this.apiManager = new ApiManager();
   }
 
-  create = semester =>
-    this.get().push({
-      ...semester
-    });
+  create = semester => {
+    return new Promise((resolve, reject) => {
+      this.apiManager.post(`${ENDPOINT.SEMESTER}`, semester)
+        .then(() => resolve())
+        .catch((err) => reject(err))
+    })
+  }
 
   get = (uid = null) => {
     if (uid == null) {
       return new Promise((resolve, reject) => {
-        this.database
-          .ref('semester')
-          .once('value')
-          .then(snapshot => {
-            const value = snapshot.val();
-
-            if (value != null) {
-              const valueList = Object.keys(value).map(key => ({
-                ...value[key],
-                uid: key
-              }));
-
-              resolve(valueList);
-            } else {
-              resolve(null);
-            }
-          })
-          .catch(error => {
-            reject(error);
-          });
-      });
+        this.apiManager.get(ENDPOINT.SEMESTER)
+          .then(semesters => resolve(semesters))
+          .catch(err => reject(err))
+      })
     } else {
       return new Promise((resolve, reject) => {
-        this.database
-          .ref('semester')
-          .once('value')
-          .then(snapshot => {
-            resolve(snapshot.val());
-          })
-          .catch(error => {
-            reject(error);
-          });
-      });
+        this.apiManager.get(`${ENDPOINT.SEMESTER} / ${uid}`)
+          .then(semesters => resolve(semesters))
+          .catch(err => reject(err))
+      })
     }
   };
 
-  update = (uid, semester) =>
-    this.get(uid).set({
-      ...semester
-    });
+  update = (uid, semester) => {
+    this.apiManager.put(`${ENDPOINT.SEMESTER}/${uid}`, semester)
+      .then(response => { })
+      .catch(err => { })
+  }
 
-  delete = uid => this.get(uid).remove();
+  delete = uid => {
+    this.apiManager.delete(`${ENDPOINT.SEMESTER}/${uid}`)
+      .then(response => {
+        callback(null)
+      })
+      .catch(err => console.log(err))
+  }
+
 }
 
 export default Semester;
