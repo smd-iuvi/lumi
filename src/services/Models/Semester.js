d12a632b04726b2b327ce156446754e4008293e5
Paulo José <paulocardosob@gmail.com>
2020-12-03 12:30:19 -0300

    Update Semester.js

--- src/services/Models/Semester.js
+++ src/services/Models/Semester.js
@@ -6,14 +6,6 @@ class Semester {
     this.apiManager = new ApiManager();
   }
 
-  // create = semester => {
-  //   return new Promise((resolve, reject) => {
-  //     this.apiManager.post(`${ENDPOINT.SEMESTER}`, semester)
-  //       .then(() => resolve())
-  //       .catch((err) => reject(err))
-  //   })
-  // }
-
   get = (uid = null) => {
     if (uid == null) {
       return new Promise((resolve, reject) => {
@@ -29,20 +21,6 @@ class Semester {
       })
     }
   };
-
-  // update = (uid, semester) => {
-  //   this.apiManager.put(`${ENDPOINT.SEMESTER}/${uid}`, semester)
-  //     .then(response => { })
-  //     .catch(err => { })
-  // }
-
-  // delete = uid => {
-  //   this.apiManager.delete(`${ENDPOINT.SEMESTER}/${uid}`)
-  //     .then(response => {
-  //       callback(null)
-  //     })
-  //     .catch(err => console.log(err))
-  // }
 
 }
 
